99c4efce4438eb64b4d754796dea2079a6cc5279
Mishaa <mishaa.pro@proton.me>
2023-01-19 18:55:05 +0100

    :heavy_plus_sign: add eslint, prettier, related recommended configuration and lint files

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,4 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <h1 class="text-red-500 text-2xl">Hello from Homepage</h1>
---
+++ stores/application.store.ts
@@ -0,0 +1,5 @@
+import { defineStore } from 'pinia'
+
+export const applicationStore = defineStore({
+  id: 'application',
+})
